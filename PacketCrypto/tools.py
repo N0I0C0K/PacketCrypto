cde1649182d40774bf4366b6062543442ef0fa15
Nick <nick131410@aliyun.com>
2022-05-06 09:37:23 +0800

    删掉了不必要的功能

--- PacketCrypto/tools.py
+++ PacketCrypto/tools.py
@@ -19,5 +19,5 @@ def generateRsaKey():
     with open(path+'\\public_key', 'wb') as file:
         print(f'[*] public key => {file.name}')
         file.write(pub.save_pkcs1())
-    print(f'{set_color("public key", COLOR.BLUE)}:\n{pub.save_pkcs1().decode()}')
-    print(f'{set_color("private key", COLOR.CYAN)}:\n{pri.save_pkcs1().decode()}')
+    print(f'public key:\n{pub.save_pkcs1().decode()}')
+    print(f'private key:\n{pri.save_pkcs1().decode()}')
--- PacketCrypto/utils.py
+++ PacketCrypto/utils.py
@@ -3,11 +3,9 @@ import os
 from typing import Union
 from pydantic import BaseModel
 
-__all__ = ['COLOR', 'set_color', 'EncryptData', 'model_path']
+__all__ = ['EncryptData', 'model_path']
 
 model_path = os.path.dirname(__file__)
-
-color_support = True
 
 
 class EncryptData(BaseModel):
@@ -15,32 +13,3 @@ class EncryptData(BaseModel):
     sign: str
     key: str
     nonce: str
-
-
-class COLOR:
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    DEFAULT = '\033[39m'
-
-
-if sys.platform == "win32":
-    try:
-        # https://stackoverflow.com/questions/36760127/...
-        # how-to-use-the-new-support-for-ansi-escape-sequences-in-the-windows-10-console
-        from ctypes import windll
-        kernel32 = windll.kernel32
-        kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
-    except Exception:  # pragma: no cover
-        color_support = False
-
-
-def set_color(s: str, color: COLOR) -> str:
-    if color_support:
-        return f"{color}{s}{COLOR.DEFAULT}"
-    return s

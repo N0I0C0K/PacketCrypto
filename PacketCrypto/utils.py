88fe6ff7bd4d92b5c6f0dc5bb5ca9b8a3f2cf043
Nick <nick131410@aliyun.com>
2022-05-15 10:27:57 +0800

    更新了数据模型

--- PacketCrypto/utils.py
+++ PacketCrypto/utils.py
@@ -1,15 +1,20 @@
 import sys
 import os
 from typing import Union
-from pydantic import BaseModel
 
 __all__ = ['EncryptData', 'model_path']
 
 model_path = os.path.dirname(__file__)
 
 
-class EncryptData(BaseModel):
+class EncryptData:
     data: str
     sign: str
     key: str
     nonce: str
+
+    @staticmethod
+    def parse_obj(obj: dict) -> 'EncryptData':
+        res = EncryptData()
+        res.__dict__.update(obj)
+        return res
